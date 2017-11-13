be5c7c51d150b61de3d50770c33d00443b618c20
Lee Dowthwaite <leedowthwaite@gmail.com>
2017-11-13 08:02:11 +0000

    Initial Commit

---
+++ LDFinCharts/LDFinCharts.h
@@ -0,0 +1,19 @@
+//
+//  LDFinCharts.h
+//  LDFinCharts
+//
+//  Created by Lee Dowthwaite on 13/11/2017.
+//  Copyright © 2017 Lee Dowthwaite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LDFinCharts.
+FOUNDATION_EXPORT double LDFinChartsVersionNumber;
+
+//! Project version string for LDFinCharts.
+FOUNDATION_EXPORT const unsigned char LDFinChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDFinCharts/PublicHeader.h>
+
+
